b77071f823d475017252c7616911f8d45cdaaa1d
MKX81 <k.mattias.karlsson@gmail.com>
2025-07-07 18:42:28 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function visaBokningsmeddelande() {
+  const meddelande = document.getElementById("bokningsmeddelande");
+  meddelande.textContent = "Tack! Vi återkommer inom kort med en bekräftelse.";
+  meddelande.style.color = "#009739";
+  meddelande.style.marginTop = "1rem";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav-links");
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+});
